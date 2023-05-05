426fb5a2835395a40685b019398f22c5e4ea7be5
Thalida Noel <hello@thalida.com>
2023-05-05 05:08:09 +0000

    Test & Lint

--- api/api/test_main.py
+++ api/api/test_main.py
@@ -3,7 +3,6 @@
 from unittest.mock import patch
 
 from fastapi.testclient import TestClient
-import pytest
 
 from .main import app
 
@@ -26,7 +25,9 @@ def test_metadata__has_data(mock_get):
         <head>
             <meta property="og:title" content="Test title">
             <meta property="og:description" content="Test description">
-            <meta property="og:image" content="https://test.local.example/image.png">
+            <meta
+                property="og:image"
+                content="https://test.local.example/image.png">
             <meta property="notog:title" content="Test notog title">
         </head>
     </html>
