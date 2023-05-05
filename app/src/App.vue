f9329e10d7c6711307f6c95dd73d5950e2b3f1d9
Thalida Noel <hello@thalida.com>
2023-05-05 06:37:36 +0000

    Cleanup e2e

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,9 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
