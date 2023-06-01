c0a72a6499454ae451412cd3e9af3276e61ce0bd
Thalida Noel <hello@thalida.com>
2023-06-01 03:40:25 +0000

    Made backgrounds gradients

--- app/src/components/SpaceLayout.vue
+++ app/src/components/SpaceLayout.vue
@@ -35,12 +35,13 @@ provide('spaceGridRef', spaceGridRef)
 <template>
   <div
     ref="spaceRef"
-    class="space-layout flex bg-sky-50 dark:bg-slate-950"
+    class="space-layout flex relative"
     :class="{
       'is-editing': spaceStore.isEditMode,
     }"
     @click="handleSpaceClick"
   >
+    <div class="space-layout__background w-full h-full fixed top-0 left-0"></div>
     <SpaceGrid
       ref="spaceGridRef"
       class="space-layout__grid"
@@ -61,4 +62,46 @@ provide('spaceGridRef', spaceGridRef)
 .is-editing .space-layout__grid {
   margin-right: 320px;
 }
+
+.space-layout__background,
+.light .space-layout__background {
+  background: linear-gradient(0deg, rgb(229 255 221) , rgb(255 255 255 / 0%));
+
+  &:before,
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  &:before {
+    background: linear-gradient(120deg, rgb(201 255 252 / 60%) , rgb(255 255 255 / 0%));
+  }
+  &:after {
+    background: linear-gradient(240deg, rgb(255 231 233 / 60%) , rgb(255 255 255 / 0%));
+  }
+}
+
+.dark .space-layout__background {
+  background: #17000f linear-gradient(0deg, rgb(3 0 34) , rgb(0 0 0 / 0%));
+
+  &:before,
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  &:before {
+    background: linear-gradient(120deg, rgb(9 0 47 / 60%) , rgb(0 0 0 / 0%))
+  }
+  &:after {
+    background: linear-gradient(240deg, rgb(22 22 45 / 60%) , rgb(0 0 0 / 0%))
+  }
+}
+
 </style>
